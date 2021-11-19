98ff3b02d3535cac0f1ac105be92c60e25f7e943
klezi10 <74952593+klezi10@users.noreply.github.com>
2021-11-19 16:45:15 +0700

    set up functionality for prev btn

--- script.js
+++ script.js
@@ -9,6 +9,13 @@
 const slides = document.getElementsByClassName('carousel-item');
 let slidePosition = 0;
 const totalSlides = slides.length;
+document
+  .getElementById('carousel-button-next')
+  .addEventListener('click', moveToNextSlide);
+
+document
+  .getElementById('carousel-button-prev')
+  .addEventListener('click', moveToPrevSlide);
 
 function hideAllSlides() {
   for (let slide of slides) {
@@ -17,16 +24,23 @@ function hideAllSlides() {
   }
 }
 
-document
-  .getElementById('carousel-button-next')
-  .addEventListener('click', moveToNextSlide);
-
 function moveToNextSlide() {
   hideAllSlides();
   if (slidePosition === totalSlides - 1) {
     slidePosition = 0;
   } else {
     slidePosition++;
+  }
+  slides[slidePosition].classList.add('carousel-item-visible');
+}
+
+function moveToPrevSlide() {
+  hideAllSlides();
+
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition--;
   }
   slides[slidePosition].classList.add('carousel-item-visible');
 }
